7500518831eb6672b8190a0005dd1ae30e0b1810
Dmitriy-OLW <124800644+Dmitriy-OLW@users.noreply.github.com>
2025-04-16 18:47:06 +0300

    Update forms.py

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 
-from .models import Post, Comment, User
+from .models import Post, Comment, User, Location  
 
 
 class PostForm(forms.ModelForm):
+    new_location = forms.CharField(
+        required=False,
+        label="Или введите новое местоположение"
+    )
 
     class Meta:
         model = Post
@@ -11,6 +15,26 @@ class PostForm(forms.ModelForm):
         widgets = {
             'pub_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['location'].required = False  
+
+    def clean(self):
+        cleaned_data = super().clean()
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        new_location = self.cleaned_data.get('new_location')
+        
+        if new_location:
+            location = Location.objects.create(name=new_location)
+            instance.location = location
+            
+        if commit:
+            instance.save()
+        return instance
 
 
 class CommentForm(forms.ModelForm):
